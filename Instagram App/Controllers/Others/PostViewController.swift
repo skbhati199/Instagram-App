6298eb92f9f4d3105ac4d48b4ed5c8cba6256030
Sonu Kumar <skbhati199@gmail.com>
2023-11-22 20:51:52 +0530

    folder and app structure

---
+++ Instagram App/Controllers/Others/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  Instagram App
+//
+//  Created by Sonu Kumar on 22/11/23.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
